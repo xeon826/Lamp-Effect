e817c4e832176a14814473cefada451a74211204
dan <josh@deltstl.com>
2018-08-02 14:30:04 -0500

    fixed x axis

--- main.js
+++ main.js
@@ -1,35 +1,49 @@
 $(document).ready(function() {
+  $('.has__lighting').each(function(i, obj) {
+    $(this).addClass('has__lighting' + i);
+    lamp($('.has__lighting' + i));
+  })
+
+  $(window).on('resize', function() {
+    $('.has__lighting').off();
+    // lamp($('.has__lighting'));
+    $('.has__lighting').each(function(i, obj) {
+      $(this).addClass('has__lighting' + i);
+      lamp($('.has__lighting' + i));
+    })
+  })
+
+});
+
+function lamp($obj) {
   var currentMousePos = {
     x: -1,
     y: -1
   };
   var scrollTop = $(window).scrollTop(),
-    top = $('.box').offset().top + $('.box').height()/2,
-    $el = $('.box');
-    distance = (top - scrollTop),
-    offset = $('.box').offset(),
-    left = offset.left + $el.width()/2,
-    right = $(window).innerWidth()- left,
-    bottom = $(window).innerHeight()-$el.height()/2- top;
-    console.log (left + ' ' + right + ' '+ top + ' ' + bottom)
-    $('.dot').css({
-      // 'left': left,
-      'right':right,
-      // 'bottom':bottom,
-      'top':top,
-    })
+    top = $obj.offset().top + $obj.height() / 2,
+    // $el = $obj;
+  distance = (top - scrollTop),
+    offset = $obj.offset(),
+    left = offset.left + $obj.width() / 2,
+    right = $(window).innerWidth() - left,
+    bottom = $(window).innerHeight() - $obj.height() / 2 - top;
+  console.log(left + ' ' + right + ' ' + top + ' ' + bottom)
+  $('.dot').css({
+    // 'left': left,
+    'right': right,
+    // 'bottom':bottom,
+    'top': top,
+  })
   $(document).mousemove(function(event) {
     currentMousePos.x = event.pageX;
     currentMousePos.y = event.pageY;
     var distY = currentMousePos.y - top,
-    distX = currentMousePos.x - left;
-    $('.box').css({
-      'box-shadow': -distX/25 + 'px ' + -distY/25+'px '+ '8px',
+      distX = currentMousePos.x - left;
+      console.log(distX + ' ' + distY);
+    $obj.css({
+      'box-shadow': -distX / 25 + 'px ' + -distY / 25 + 'px ' + '5px',
     })
   });
 
-  // ELSEWHERE, your code that needs to know the mouse position without an event
-  if (currentMousePos.x < 10) {
-    // ....
-  }
-});
+}
